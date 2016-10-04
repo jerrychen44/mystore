3f3fd3de4bf575341953f63fe16b085f8da87d74
jerrychen <jerrychen040@gmail.com>
2016-10-04 10:13:47 +0800

    integrate our store page and copy the template from catalog.html part1

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
 class PagesController < ApplicationController
   def index
+    # [Jerry] add for Store page
+    @product = Product.all
+  end
+
+  # [Jerry] add for Store page
+  def shop
+    @product = Product.all
   end
 end
