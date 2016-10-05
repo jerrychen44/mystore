a36c4d6a4e2280463d61d833bf34aaac3ec61ff4
jerrychen <jerrychen040@gmail.com>
2016-10-05 10:42:55 +0800

    keep implement the view and commit before use toastr-rails gem

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
+
+  # under /controllers/concerns/current_cart.rb
+  include CurrentCart
+  before_action :set_cart, only: [:index, :shop]
+
   def index
     # [Jerry] add for Store page
     @product = Product.all
